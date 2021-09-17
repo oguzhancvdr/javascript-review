bbd2441449cffcbc7a5c11f099cf68c45668b015
ogzhn <oguzhancavdar06@gmail.com>
2021-09-17 22:37:04 +0300

    create app.js for review objects

---
+++ OOP/objects/app.js
@@ -0,0 +1,20 @@
+let val;
+
+let num = 10;
+
+// object literal
+let obi = {
+    name: "obi",
+    yearOfBirth: 2010,
+    job: "dev"
+}
+
+//array
+let numbers = [10,20,30]
+
+val = num;
+val = obi;
+val = numbers;
+
+console.log(val);
+console.log('typeof :>> ', typeof val);
