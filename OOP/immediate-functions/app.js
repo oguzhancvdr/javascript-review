58f22b898c1f71e1aae7a38439903808723f0b80
ogzhn <oguzhancavdar06@gmail.com>
2021-09-21 23:22:20 +0300

    add immidiate functions lesson

---
+++ OOP/immediate-functions/app.js
@@ -0,0 +1,26 @@
+// Immidiate functions
+
+// ? Two types of immidiate function declaretaions
+
+// (function(){
+
+// })();
+
+// (function(){
+
+// }());
+
+(function (name) {
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var today = new Date();
+  var msg = "Welcome " + name + ", Today is " + days[today?.getDay()];
+  console.log("msg :>> ", msg);
+})('Obi');
