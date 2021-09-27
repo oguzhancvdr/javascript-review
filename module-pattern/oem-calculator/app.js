d176a14a115adf8eadd3f0337d40da788eec3964
ogzhn <oguzhancavdar06@gmail.com>
2021-09-27 15:17:50 +0300

    create data structure

--- module-pattern/oem-calculator/app.js
+++ module-pattern/oem-calculator/app.js
@@ -4,16 +4,54 @@ const StorageController = (function () {})();
 // Product Controller
 const ProductController = (function () {
   // * private
+  class Product {
+    constructor(id, name, price) {
+      this.id = id;
+      this.name = name;
+      this.price = price;
+    }
+  }
+
+  const data = {
+    products: [
+      { id: 0, name: "Screen", price: 200 },
+      { id: 1, name: "RAM", price: 150 },
+      { id: 2, name: "Keyboard", price: 15 },
+    ],
+    selectedProduct: null,
+    total: 0,
+  };
 
   // * public
   return {
+    getProducts: function () {
+      return data.products;
+    },
 
-  }
+    getData: function () {
+      return data;
+    },
+  };
 })();
 
 // UI Controller
 const UIController = (function () {})();
 
 // App Controller
-const AppController = (function (ProductCtrl, UICtrl) {
-})(ProductController, UIController);
+const App = (function (ProductCtrl, UICtrl) {
+
+
+
+  return {
+    init : function(){
+      console.log("starting app...");
+      const products = ProductCtrl.getProducts();
+      // UICtrl.createProductList(products);
+    }
+  }
+})(
+  ProductController,
+  UIController
+);
+
+App.init();
