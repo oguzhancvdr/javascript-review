50a1bc495a3d2984bc4e16901c84754ada31f980
ogzhn <oguzhancavdar06@gmail.com>
2021-10-08 00:08:51 +0300

    add unary, binary and ternary operators lesson

---
+++ js-review-continued/ternary-operator/app.js
@@ -0,0 +1,13 @@
+// unary operator - typeof
+let text = 'some text';
+console.log(typeof text); // operand
+
+// binary operator - assignment
+let number = 3;
+let number2 = 2 + 5;
+
+// ternary operator
+// condition ? (runs if true) : (runs if false)
+const value = 2 < 1;
+
+value ? console.log("value is true") : console.log('value is false');
