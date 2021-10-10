bc178419ab04561abd29b947d68dcbfb28c342d9
ogzhn <oguzhancavdar06@gmail.com>
2021-10-10 17:56:22 +0300

    add filter and find method implementation

--- js-review-continued/array-iterators/callbacks-HOF/app.js
+++ js-review-continued/array-iterators/callbacks-HOF/app.js
@@ -12,15 +12,15 @@ for (let i = 0; i < numbers.length; i++) {
 }
 console.log('**************************************************')
 
+const people = [
+  { name: 'bob', age: 20, position: 'developer', id: 1 },
+  { name: 'peter', age: 25, position: 'designer', id: 2 },
+  { name: 'susy', age: 30, position: 'the CTO', id: 3 },
+  { name: 'anna', age: 35, position: 'the CEO', id: 4 },
+]
+
 // * forEach
 // ! does not return new array
-
-const people = [
-  { name: 'bob', age: 20, position: 'developer' },
-  { name: 'peter', age: 25, position: 'designer' },
-  { name: 'susy', age: 30, position: 'the CTO' },
-  { name: 'anna', age: 35, position: 'the CEO' },
-]
 
 // callback funtion of forEach
 function showPerson(person) {
@@ -52,11 +52,51 @@ const newPeople = people.map(function (person) {
   }
 })
 
-const names = people.map(function(person){
+const names = people.map(function (person) {
   return `<h1>${person.name}</h1>`
 })
 
 // we used join with empty sting to get rid of single quote of <h1></h1> tags
-document.body.innerHTML = names.join('');
+document.body.innerHTML = names.join('')
 console.log(names)
+console.log('**************************************************')
 
+// * filter
+// ! does not return a new array
+// ? can manipulate the size of new array
+// ? returns based on condition
+
+const youngPeople = people.filter(function (person) {
+  return person.age <= 25
+})
+
+const developers = people.filter(function (person) {
+  return person.position === 'developer'
+})
+
+console.log(`youngPeople`, youngPeople)
+console.log(`developers`, developers)
+console.log('**************************************************')
+
+// * find
+// ? returns single instance - (in this case object)
+// ! returns first match, if no match undefined
+// ? great for getting unique value
+
+const _names = ['bob', 'peter', 'susy']
+console.log(
+  _names.find(function (name) {
+    // if no matched name it gives undefined not an error
+    return name === 'bob'
+  })
+)
+
+const person = people.find(function (person) {
+  return person.id === 3
+})
+console.log('person with filter :', person.name)
+
+const person2 = people.filter(function (person) {
+  return person.id === 3
+})
+console.log('person2 with filter : ', person2[0].name)
