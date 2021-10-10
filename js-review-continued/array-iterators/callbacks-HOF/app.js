cb4a52d69a61f400bfb55123a6eef81967c281bf
ogzhn <oguzhancavdar06@gmail.com>
2021-10-10 18:10:42 +0300

    add reduce func

--- js-review-continued/array-iterators/callbacks-HOF/app.js
+++ js-review-continued/array-iterators/callbacks-HOF/app.js
@@ -13,10 +13,10 @@ for (let i = 0; i < numbers.length; i++) {
 console.log('**************************************************')
 
 const people = [
-  { name: 'bob', age: 20, position: 'developer', id: 1 },
-  { name: 'peter', age: 25, position: 'designer', id: 2 },
-  { name: 'susy', age: 30, position: 'the CTO', id: 3 },
-  { name: 'anna', age: 35, position: 'the CEO', id: 4 },
+  { name: 'bob', age: 20, position: 'developer', id: 1, salary: 200 },
+  { name: 'peter', age: 25, position: 'designer', id: 2, salary: 300 },
+  { name: 'susy', age: 30, position: 'the CTO', id: 3, salary: 500 },
+  { name: 'anna', age: 35, position: 'the CEO', id: 4, salary: 500 },
 ]
 
 // * forEach
@@ -100,3 +100,23 @@ const person2 = people.filter(function (person) {
   return person.id === 3
 })
 console.log('person2 with filter : ', person2[0].name)
+console.log('**************************************************')
+
+// * reduce
+// ? iterates, callback function
+// ? reduces to a single value - number, array, object
+// ? 1 paramater ('acc') - total of all calculations
+// ? 2 parameter ('curr') - current iteration/value
+
+// important thing is here that what value will be returned ?
+// number ? array ? object ? in our case we will return number so we passed 0(zero)
+const total = people.reduce(function (acc, currItem) {
+  console.log(`total ${acc}`)
+  console.log(`current money:  ${currItem.salary}`)
+  acc += currItem.salary
+
+  // ! always always always return acc not curr item!
+  return acc
+}, 0)
+
+console.log(total)
