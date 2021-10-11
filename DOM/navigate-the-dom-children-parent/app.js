95e3b5ab90a3d102bca3c8e57199b397b94ceda7
ogzhn <oguzhancavdar06@gmail.com>
2021-10-11 23:25:10 +0300

    add previousElementSibling and next one

--- DOM/navigate-the-dom-children-parent/app.js
+++ DOM/navigate-the-dom-children-parent/app.js
@@ -5,22 +5,22 @@
 // lastChild
 
 const result = document.querySelector('#result')
-const allChildren = result.childNodes;
-console.log(allChildren);
+const allChildren = result.childNodes
+console.log(allChildren)
 
 // * children
 const children = result.children
-console.log(children);
+console.log(children)
 
 // * firstChild
-console.log(result.firstChild);
-console.log(result.lastChild);
+console.log(result.firstChild)
+console.log(result.lastChild)
 
 // * parent element
 
 const heading = document.querySelector('h2')
-console.log(heading.parentElement.parentElement);
-const parent = heading.parentElement;
+console.log(heading.parentElement.parentElement)
+const parent = heading.parentElement
 parent.style.color = 'red'
 
 // previosSibling
@@ -30,6 +30,16 @@ const first = document.querySelector('.first')
 const second = first.nextSibling.nextSibling.style.color = 'red '
 
 const last = document.querySelector('#last')
-const third = last.previousSibling.previousSibling
+const fifth = last.previousSibling.previousSibling
 
-console.log(third);
+console.log(fifth)
+
+// previousElementSibling
+// nextElementSibling
+
+const _first = document.querySelector('.first')
+// we dont need to think whitespace anymore
+const sec = _first.nextElementSibling.style.color = 'blue'
+
+const _last = document.querySelector('#last')
+const _fifth = _last.previousElementSibling.style.color = 'red'
