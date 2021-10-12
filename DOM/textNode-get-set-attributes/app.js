0e4813b84970d00c17be90b50b230289ee8bfdf6
ogzhn <oguzhancavdar06@gmail.com>
2021-10-12 23:23:21 +0300

    add get-set attribute and insertBefore lesson

---
+++ DOM/textNode-get-set-attributes/app.js
@@ -0,0 +1,27 @@
+// nodeValue
+// textContent
+
+const item = document.getElementById('text')
+const value = item.firstChild.nodeValue
+const easyValue = item.textContent
+console.log({easyValue, value})
+
+// getAttribute()
+// setAttribute()
+
+const first = document.querySelector('.first')
+const classValue = first.getAttribute('class')
+const id = first.getAttribute('id')
+console.log({classValue, id});
+
+const link = document.getElementById('link')
+const showLink = link.getAttribute('href')
+console.log(showLink)
+
+const afterLinkElement = link.nextElementSibling
+afterLinkElement.setAttribute('class', 'first')
+afterLinkElement.textContent = 'I also have a class of first'
+console.log(afterLinkElement);
+
+const links = document.querySelectorAll('.first')
+console.log(links)
