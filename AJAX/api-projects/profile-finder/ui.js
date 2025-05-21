9794ef64a0c5e6f8fe5e564fbada4d60fd6b3761
ogzhncvdr <oguzhancavdar06@gmail.com>
2025-05-21 16:05:33 +0300

    remove unbroken images element

--- AJAX/api-projects/profile-finder/ui.js
+++ AJAX/api-projects/profile-finder/ui.js
@@ -8,9 +8,6 @@ class UI {
     this.profileContainer.innerHTML = `
     <div class="card card-body">
       <div class="row"> 
-        <div class="col-md-3">       
-          <a href="https://placeholder.com"><img class="img-thumbnail" src="https://via.placeholder.com/150"></a>
-        </div>
         <div class="col-md-9">
           <h4>Contact</h4>
           <ul class="list-group">
