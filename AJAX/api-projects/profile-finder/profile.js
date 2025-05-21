6208e25dd58c6beceb8d4ebca32aeadf17d560cb
ogzhncvdr <oguzhancavdar06@gmail.com>
2025-05-21 16:07:21 +0300

    update api uri according to document of jsonplaceholder

--- AJAX/api-projects/profile-finder/profile.js
+++ AJAX/api-projects/profile-finder/profile.js
@@ -2,14 +2,20 @@ class Profile {
   constructor() {
     this.clientId = "";
     this.clientSecret = "";
+    this.timeoutId;
   }
 
-  async getProfile(username) {
-    const url = `https://jsonplaceholder.typicode.com/users?username=${username}`;   
+  _debounce(callback, delay) {
+    clearTimeout(this.timeoutId);
+    this.timeoutId = setTimeout(callback, delay);
+  }
+
+  async getProfile(userID) {
+    const url = `https://jsonplaceholder.typicode.com/users/${userID}`;
     const profileResponse = await fetch(url);
     const profile = await profileResponse.json();
 
-    const todoUrl = `https://jsonplaceholder.typicode.com/todos/?userId=${profile?.[0]?.id}`;
+    const todoUrl = `https://jsonplaceholder.typicode.com/users/${profile?.id}/todos`;
     const todoResponse = await fetch(todoUrl);
     const todo = await todoResponse.json();
 
